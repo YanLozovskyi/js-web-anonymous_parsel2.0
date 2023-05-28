a3d3c8115f1369f35554a134a0e926c35176dc99
Taras <117749635+vasylkivt@users.noreply.github.com>
2023-05-28 17:14:17 +0300

    finish

--- src/js/UnsplashAPI.js
+++ src/js/UnsplashAPI.js
@@ -6,15 +6,39 @@ import axios from 'axios';
 export class UnsplashAPI {
   #BASE_URL = 'https://api.unsplash.com';
   #API_KEY = 'LxvKVGJqiSe6NcEVZOaLXC-f2JIIWZaq_o0WrF8mwJc';
+  #query = '';
+
+  constructor(perPage = 12) {
+    this.per_page = perPage;
+  }
 
   getPopularPhotos(page) {
     return axios.get(`${this.#BASE_URL}/search/photos`, {
       params: {
         query: 'random',
         page,
-        per_page: 12,
+        per_page: this.per_page,
         client_id: this.#API_KEY,
       },
     });
+  }
+
+  getPhotosByQuery(page) {
+    return axios.get(`${this.#BASE_URL}/search/photos`, {
+      params: {
+        query: this.#query,
+        page,
+        per_page: this.per_page,
+        client_id: this.#API_KEY,
+      },
+    });
+  }
+
+  get query() {
+    return this.#query;
+  }
+
+  set query(newQuery) {
+    this.#query = newQuery;
   }
 }
